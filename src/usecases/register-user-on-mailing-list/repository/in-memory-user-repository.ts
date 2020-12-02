2ab991b77b4e69814386c8371b04426313010009
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-02 19:14:21 -0300

    feat: add, exists and find by email implemented

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -9,19 +9,27 @@ export class InMemoryUserRepository implements UserRepository {
     this.repository = users
   }
 
-  add (user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      (this.repository).push(user)
+    }
   }
 
-  findUserByEmail (email: string): Promise<UserData> {
-    return null
+  async findUserByEmail (email: string): Promise<UserData> {
+    const found = this.repository.find(user => user.email === email)
+    return found || null
   }
 
   findAllUsers (): Promise<UserData[]> {
     throw new Error('Method not implemented.')
   }
 
-  exists (user: UserData): Promise<Boolean> {
-    throw new Error('Method not implemented.')
+  async exists (user: UserData): Promise<Boolean> {
+    if (await this.findUserByEmail(user.email) == null) {
+      return false
+    }
+
+    return true
   }
 }
