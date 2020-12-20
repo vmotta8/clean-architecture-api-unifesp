4daaea9ff86805689c442cd714f9b6f89e8ebdbf
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-20 13:46:55 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -7,5 +8,23 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalidEmail'
     const error = User.create({ name: 'My Name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  it('should not create user with invalid name too few', () => {
+    const invalidName = 'O     '
+    const error = User.create({
+      name: invalidName,
+      email: 'myname@email.com'
+    })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  it('should not create user with invalid name too many', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({
+      name: invalidName,
+      email: 'myname@email.com'
+    })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
