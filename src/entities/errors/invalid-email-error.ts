81e64404dfa839330bd30d6862b16867af883077
Vinicius Motta <viniciusmotta8@gmail.com>
2021-01-19 21:45:56 -0300

    feat: improve error classes

--- src/entities/errors/invalid-email-error.ts
+++ src/entities/errors/invalid-email-error.ts
@@ -1,3 +1,7 @@
 export class InvalidEmailError extends Error {
-  //
+  public readonly name = 'InvalidEmailError'
+
+  constructor (invalidEmail: string) {
+    super('Invalid email: ' + invalidEmail)
+  }
 }
