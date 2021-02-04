ba5461cf8a4d9f229fa566d5a2d614cfebc86229
Vinicius Motta <viniciusmotta8@gmail.com>
2021-02-03 21:15:36 -0300

    feat: web controller should return 201 when user is created

---
+++ src/controllers/register-user-controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-useless-constructor */
+import { UserData } from '@/entities'
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { HttpRequest } from './ports'
+import { created } from './util'
+
+export class RegisterUserController {
+  constructor (
+    private readonly usecase: RegisterUserOnMailingList
+  ) {}
+
+  public async handle (request: HttpRequest) {
+    const userData: UserData = request.body
+    const response = await this.usecase.registerUserOnMailingList(userData)
+
+    if (response.isRight()) {
+      return created(response.value)
+    }
+  }
+}
