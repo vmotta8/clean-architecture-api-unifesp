7f3fbde5390de35d58fa81038e37122af8067e0a
Vinicius Motta <viniciusmotta8@gmail.com>
2021-02-10 18:52:34 -0300

    test: add content type middleware

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('content type middleware', () => {
+  it('should return a default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  it('should return a xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
